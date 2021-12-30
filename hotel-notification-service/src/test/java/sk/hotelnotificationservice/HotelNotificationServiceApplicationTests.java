b3f00a8ea8fd76f35ddccfe5b971c71042d6d28e
estojanovic <estojanovic8720rn@raf.rs>
2021-12-30 20:05:29 +0100

    initial commit

---
+++ hotel-notification-service/src/test/java/sk/hotelnotificationservice/HotelNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.hotelnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ hotel-user-service/src/test/java/sk/hoteluserservice/HotelUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.hoteluserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
