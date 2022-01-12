fe3e4b5a88f60bdec7b2959dbf616b6f6f2e59b9
Aleksa <adjokic1619rn@raf.rs>
2022-01-12 22:35:24 +0100

    Dependency

--- hotel-api-getaway/src/main/java/sk/hotelapigetaway/HotelApiGetawayApplication.java
+++ hotel-api-getaway/src/main/java/sk/hotelapigetaway/HotelApiGetawayApplication.java
@@ -1,16 +1,12 @@
 package sk.hotelapigetaway;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-
 
 
 @SpringBootApplication
@@ -27,6 +23,9 @@ public class HotelApiGetawayApplication {
 		config.addAllowedMethod("*");
 		source.registerCorsConfiguration("/**", config);
 		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+
+
+
 		bean.setOrder(0);
 		return bean;
 	}
